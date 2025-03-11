11f3c40644fe082355e243a6cd613eebea38c64d
Theosly <theoamagalhaespc@gmail.com>
2025-03-11 11:24:37 -0300

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-g++ -O3 -Wall -Wextra ./render/camera.cpp ./render/scene.cpp ./render/texture.cpp ./render/objects/cilinder.cpp ./render/objects/cone.cpp ./render/objects/mesh.cpp ./render/objects/plane.cpp ./render/objects/sphere.cpp ./math/vec3.cpp ./main.cpp -IC:/SDL2/include -LC:/SDL2/lib -lSDL2 -lSDL2_image -o main.exe
+g++ -O3 -Wall -Wextra ./render/camera.cpp ./render/scene.cpp ./render/texture.cpp ./render/objects/cilinder.cpp ./render/objects/cone.cpp ./render/objects/mesh.cpp ./render/objects/plane.cpp ./render/objects/sphere.cpp ./render/pick.cpp ./math/vec3.cpp ./main.cpp -IC:/SDL2/include -LC:/SDL2/lib -lSDL2 -lSDL2_image -o main.exe
 main.exe
--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <cmath>
 #include <chrono>
 #include <SDL.h>
+#include <iostream>
+#include <limits>
 
 #include "SDL_events.h"
 #include "SDL_keycode.h"
@@ -25,8 +27,7 @@
 #include "render/objects/cilinder.hpp"
 #include "render/objects/cone.hpp"
 #include "render/texture.hpp"
-
-
+#include "render/pick.hpp"
 
 using namespace std;
 
@@ -35,19 +36,19 @@ double degreesToRadians(double degrees) {
 }
 
 int main() {
-    //posição de camera inicial para estar dentro do quarto da cena:
-    Vec3 p0 = Vec3(5,2,8);
-    
-    //definições gerais da janela
-    double aspect_ratio = 16.0/9.0;
+    // Posição de câmera inicial para estar dentro do quarto da cena:
+    Vec3 p0 = Vec3(5, 2, 8);
+
+    // Definições gerais da janela
+    double aspect_ratio = 16.0 / 9.0;
     double viewport_width = 6.4;
-    double viewport_height = viewport_width/aspect_ratio;
+    double viewport_height = viewport_width / aspect_ratio;
     double viewport_distance = 2.0;
     int image_width = 960;
-    int image_height = image_width/aspect_ratio;
+    int image_height = image_width / aspect_ratio;
     Vec3 bg_color = Vec3(0.0, 0.0, 0.0);
-    
-    // Definindo os materais a serem usados na cena:
+
+    // Definindo os materiais a serem usados na cena:
     Material parede_mat(Vec3(0.5, 0.5, 0.5), Vec3(0.6, 0.6, 0.6), Vec3(0.2, 0.2, 0.2), 10.0);
     Material chao_mat(Vec3(0.3, 0.3, 0.3), Vec3(0.5, 0.5, 0.5), Vec3(0.1, 0.1, 0.1), 5.0);
     Material ceu_mat(Vec3(0, 0, 0), Vec3(0, 0, 0), Vec3(0, 0, 0), 1.0);
@@ -80,41 +81,31 @@ int main() {
     double y5 = -0.381966; // calculado pelo desmos
 
     vector<Vec3> vertices = {
-        Vec3(0,0,0.25), // centro gordinho 1
-        Vec3 ( 0.0, 1.0, 0.0 ),
-        Vec3 ( x1, y1, 0.0 ),
-        Vec3 ( x2, y2, 0.0 ),
-        Vec3 ( -x2, y2, 0.0 ),
-        Vec3 ( -x1, y1, 0.0 ),
-        Vec3 ( 0, 0, -0.25 ), // centro gordinho 2
-        Vec3 ( x3, y3, 0), // pentagono interior direita cima
-        Vec3 ( x4, y4, 0), // pentagono interior direita baixo
-        Vec3 ( 0, y5, 0), // pentagono interior baixo
-        Vec3 ( -x4, y4, 0), // pentagono interior esq baixo
-        Vec3 ( -x3, y3, 0), // pentagono interior esq cima
+        Vec3(0, 0, 0.25), // centro gordinho 1
+        Vec3(0.0, 1.0, 0.0),
+        Vec3(x1, y1, 0.0),
+        Vec3(x2, y2, 0.0),
+        Vec3(-x2, y2, 0.0),
+        Vec3(-x1, y1, 0.0),
+        Vec3(0, 0, -0.25), // centro gordinho 2
+        Vec3(x3, y3, 0), // pentagono interior direita cima
+        Vec3(x4, y4, 0), // pentagono interior direita baixo
+        Vec3(0, y5, 0), // pentagono interior baixo
+        Vec3(-x4, y4, 0), // pentagono interior esq baixo
+        Vec3(-x3, y3, 0), // pentagono interior esq cima
     };
 
     vector<array<size_t, 3>> triangles = {
         {0, 1, 11}, {0, 7, 1}, {0, 2, 7}, {0, 8, 2}, {0, 3, 8}, {0, 9, 3}, {0, 4, 9}, {0, 10, 4}, {0, 5, 10}, {0, 11, 5},
-        {6,11, 1}, {6, 1, 7}, {6, 7, 2}, {6, 2, 8}, {6, 8, 3}, {6, 3, 9}, {6, 9, 4}, {6, 4, 10}, {6, 10, 5}, {6, 5, 11}
+        {6, 11, 1}, {6, 1, 7}, {6, 7, 2}, {6, 2, 8}, {6, 8, 3}, {6, 3, 9}, {6, 9, 4}, {6, 4, 10}, {6, 10, 5}, {6, 5, 11}
     };
-    
+
     Mesh* star = new Mesh(vertices, triangles, mat_star);
 
-    // Cubo obsoleto: 
-    // Mesh* cube = Mesh::cube(mat_sphere);
-    // cube->translate(Vec3(-0.0,-1.0,0));
-    // Mesh* cube = new Mesh("teapot400.obj", mat_sphere);
-    // cube->transform( // transforma num retangulinho girado
-    //     TransformationMatrix::rotation_around_axis(Vec3::AXIS_Y, M_PI/4.0)
-    //     * TransformationMatrix::scale_matrix(2.0, 0.5, 0.5) 
-    // );
-
-    
     // Definindo algumas luzes:
     Light point_light = Light::point(
-        Vec3(5, 8, 5), 
-        Vec3(0.6, 0.6, 0.6), 
+        Vec3(5, 8, 5),
+        Vec3(0.6, 0.6, 0.6),
         1
     );
 
@@ -126,7 +117,6 @@ int main() {
         1.0
     );
 
-
     Light directional_light = Light::directional(
         Vec3(0.0, 1.0, 0.0),
         Vec3(1.0, 1.0, 1.0),
@@ -136,51 +126,42 @@ int main() {
     Vec3 ambient_light = Vec3(0.2, 0.2, 0.2); // propriedade da cena
 
     Camera camera = Camera(p0, viewport_width, viewport_height, image_width, image_height, viewport_distance, bg_color);
-    //Definindo cena:
-    //TO-DOS: 
-    // Ajustar as bolas e a textura tanto dos planos quanto das esferas
-    // A arvore ainda está flutuando!
-    // Decidir como será a iluminação
-    // Um cubo.. presente.. será?
     Scene scene = Scene(ambient_light);
-    
-     // Estrutura da casa: 
+
+    // Estrutura da casa:
     scene.add_object(new Plane(Vec3(5, 0, 5), Vec3(0, 1, 0), chao_mat)); // chão
     scene.add_object(new Plane(Vec3(0, 2.5, 5), Vec3(1, 0, 0), parede_mat)); // parede esquerda
     scene.add_object(new Plane(Vec3(10, 2.5, 5), Vec3(-1, 0, 0), parede_mat)); // parede direita
     scene.add_object(new Plane(Vec3(5, 2.5, 0), Vec3(0, 0, 1), parede_mat)); // parede de trás
-    scene.add_object(new Plane(Vec3(5, 2.5, 10), Vec3(0, 0, -1), parede_mat)); // parede da frente 
+    scene.add_object(new Plane(Vec3(5, 2.5, 10), Vec3(0, 0, -1), parede_mat)); // parede da frente
 
     // teto/ceu
     scene.add_object(new Plane(Vec3(5, 10, 5), Vec3(0, -1, 0), ceu_mat));
- 
-     // arvore de natal
-     scene.add_object(new Cilinder(0.5, 1, Vec3(5, 0., 5), Vec3(0, 1, 0), arvore_base_mat, true, true)); // tronco da arvore
-     scene.add_object(new Cone(1.0, 1.5, Vec3(5, 3, 5), Vec3(0, 1, 0), arvore_mat, true)); // arvore
-     scene.add_object(new Cone(1.25, 2, Vec3(5, 2, 5), Vec3(0, 1, 0), arvore_mat, true)); // arvore
-     scene.add_object(new Cone(1.5, 2.5, Vec3(5, 1, 5), Vec3(0, 1, 0), arvore_mat, true)); // arvore
 
-     // bolas!
-     scene.add_object(new Sphere(Vec3(4.5, 1.4, 6.1), 0.2, bola_verme_mat));
-     scene.add_object(new Sphere(Vec3(5.5, 3, 5.8), 0.2, bola_azul_mat));
-     scene.add_object(new Sphere(Vec3(5, 2.2, 6), 0.2, bola_amar_mat));
-    
+    // arvore de natal
+    scene.add_object(new Cilinder(0.5, 1, Vec3(5, 0., 5), Vec3(0, 1, 0), arvore_base_mat, true, true)); // tronco da arvore
+    scene.add_object(new Cone(1.0, 1.5, Vec3(5, 3, 5), Vec3(0, 1, 0), arvore_mat, true)); // arvore
+    scene.add_object(new Cone(1.25, 2, Vec3(5, 2, 5), Vec3(0, 1, 0), arvore_mat, true)); // arvore
+    scene.add_object(new Cone(1.5, 2.5, Vec3(5, 1, 5), Vec3(0, 1, 0), arvore_mat, true)); // arvore
+
+    // bolas!
+    scene.add_object(new Sphere(Vec3(4.5, 1.4, 6.1), 0.2, bola_verme_mat));
+    scene.add_object(new Sphere(Vec3(5.5, 3, 5.8), 0.2, bola_azul_mat));
+    scene.add_object(new Sphere(Vec3(5, 2.2, 6), 0.2, bola_amar_mat));
+
     // Aplicando translação na estrela para posiciona-la na arvore
     scene.add_object(star);
     star->translate(Vec3(5, 4.5, 5));
-    scene.add_object(star);
 
     // Luz
-    //scene.add_light(&point_light);
     scene.add_light(&spotlight);
 
     // Olhando para a estrela
-    camera.look_at(Vec3(5,4.5,5), Vec3::AXIS_Y);
-
+    camera.look_at(Vec3(5, 4.5, 5), Vec3::AXIS_Y);
 
     // SDL init
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Trabalho FInal - Computação Gráfica", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, image_width, image_height, 0);
+    SDL_Window* window = SDL_CreateWindow("Trabalho Final - Computação Gráfica", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, image_width, image_height, 0);
     SDL_Surface* surface = SDL_GetWindowSurface(window);
 
     // main loop
@@ -200,16 +181,16 @@ int main() {
                         camera.translate(camera.coord_system[2] * -0.1);
                         break;
                     case SDLK_s:
-                        camera.translate(camera.coord_system[2] *  0.1);
+                        camera.translate(camera.coord_system[2] * 0.1);
                         break;
                     case SDLK_a:
                         camera.translate(camera.coord_system[0] * -0.1);
                         break;
                     case SDLK_d:
-                        camera.translate(camera.coord_system[0] *  0.1);
+                        camera.translate(camera.coord_system[0] * 0.1);
                         break;
                     case SDLK_SPACE:
-                        camera.translate(Vec3::AXIS_Y *  0.1);
+                        camera.translate(Vec3::AXIS_Y * 0.1);
                         break;
                     case SDLK_LSHIFT:
                         camera.translate(Vec3::AXIS_Y * -0.1);
@@ -251,17 +232,71 @@ int main() {
                         camera.projection_type = Camera::OBLIQUE;     // muda a projeção pra obliqua
                         break;
                 }
-            } else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
-                int x = event.button.x;
-                int y = event.button.y;
-                Intersection i = camera.send_ray(scene, x, y);
-                if (i.t != INFINITY) {
-                    camera.look_at(i.p, Vec3::AXIS_Y); // olha pro ponto que o usuario clicou
-                    // scene.add_object(new Sphere(i.p, 0.25, mat_sphere)); // adiciona uma esfera onde o raio bateu
+            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX = event.button.x;
+                int mouseY = event.button.y;
+
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    Intersection i = camera.send_ray(scene, mouseX, mouseY);
+                    if (i.t != INFINITY) {
+                        camera.look_at(i.p, Vec3::AXIS_Y); // olha pro ponto que o usuario clicou
+                    }
+                } else if (event.button.button == SDL_BUTTON_RIGHT) {
+                    // Usa a função pick para detectar colisão
+                    Object* selectedObject = pick(camera, scene, mouseX, mouseY);
+
+                    if (selectedObject != nullptr) {
+                        std::cout << "Objeto selecionado!" << std::endl;
+
+                        // menu de transformações
+                        std::cout << "Escolha a transformação" << std::endl;
+                        std::cout << "1. Translação" << std::endl;
+                        std::cout << "2. Rotação" << std::endl;
+                        std::cout << "3. Cancelar" << std::endl;
+
+                        int choice;
+                        std::cin >> choice;
+                        std::cin.clear(); // Corrige erro de leitura
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                        switch (choice) {
+                            case 1: { // Translação
+                                double tx, ty, tz;
+                                std::cout << "Digite os valores de translação (x y z): ";
+                                std::cin >> tx >> ty >> tz;
+                                selectedObject->translate(Vec3(tx, ty, tz));
+                                std::cout << "Translação aplicada!" << std::endl;
+                                break;
+                            }
+
+                            case 2: { // Rotação
+                                double angle;
+                                double ax, ay, az;
+                                std::cout << "Digite o eixo de rotação (x y z): ";
+                                std::cin >> ax >> ay >> az; 
+                                std::cout << "Digite o ângulo de rotação (em graus): ";
+                                std::cin >> angle;
+                                double angleRadians = degreesToRadians(angle);
+                                TransformationMatrix m = TransformationMatrix::rotation_around_axis(Vec3(ax,ay,az), angleRadians);
+                                selectedObject->transform(m);
+                                std::cout << "Rotação aplicada!" << std::endl;
+                                break;
+                            }
+                            case 3: // Cancelar
+                                std::cout << "Operação cancelada." << std::endl;
+                                break;
+                            default:
+                                std::cout << "Opção inválida!" << std::endl;
+                                break;
+                        }
+
+                    } else {
+                        std::cout << "Nenhum objeto selecionado!" << std::endl;
+                    }
                 }
             }
         }
-        
+
         auto startTime = std::chrono::high_resolution_clock::now();
         camera.draw_scene(surface, scene);
         SDL_UpdateWindowSurface(window);
@@ -271,15 +306,13 @@ int main() {
         auto printTime = endTime - fpsTimer;
         if (printTime >= std::chrono::seconds(1)) {
             std::chrono::duration<double> lastFrameTime = endTime - startTime;
-            printf("FPS: %.1f | frame_time: %.2fs\n", 1.0/lastFrameTime.count(), lastFrameTime.count());
+            printf("FPS: %.1f | frame_time: %.2fs\n", 1.0 / lastFrameTime.count(), lastFrameTime.count());
             fpsTimer = std::chrono::high_resolution_clock::now(); // reseta o timer
         }
     }
     quit:
-
     SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
 }
-        
